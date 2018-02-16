2d89706c0b4baa2bacf4003277e09f3222096800
dpaula <fernando.dpaula@gmail.com>
2018-02-16 11:57:11 -0200

    utilizando post para o cadastro de novas empresas

--- src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
@@ -12,23 +12,38 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.Empresa;
 import br.com.alura.gerenciador.dao.EmpresaDAO;
 
-@WebServlet(urlPatterns="/novaEmpresa")
-public class NovaEmpresa extends HttpServlet{
-	
+@WebServlet(urlPatterns = "/novaEmpresa")
+public class NovaEmpresa extends HttpServlet {
+
+//	@Override
+//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//
+//		String nome = req.getParameter("nome");
+//
+//		Empresa novaEmpresa = new Empresa(nome);
+//
+//		new EmpresaDAO().adiciona(novaEmpresa);
+//
+//		PrintWriter writer = resp.getWriter();
+//		writer.println("<html><body>");
+//		writer.println("<h2>Empresa adicionada com sucesso: " + novaEmpresa.getNome() + "</h2>");
+//		writer.println("</body></html>");
+//
+//	}
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
 		String nome = req.getParameter("nome");
-		
+
 		Empresa novaEmpresa = new Empresa(nome);
-		
+
 		new EmpresaDAO().adiciona(novaEmpresa);
-		
+
 		PrintWriter writer = resp.getWriter();
 		writer.println("<html><body>");
-		writer.println("<h2>Empresa adicionada com sucesso: "+novaEmpresa.getNome()+"</h2>");
+		writer.println("<h2>Empresa adicionada com sucesso: " + novaEmpresa.getNome() + "</h2>");
 		writer.println("</body></html>");
-		
 	}
 
 }
