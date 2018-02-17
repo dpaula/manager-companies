cf528008a6150151b44c26ef7b8f5cfb5119a58a
dpaula <fernando.dpaula@gmail.com>
2018-02-17 10:22:52 -0200

    testando a criação/inicialização do servlet e o seu destrou

--- src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -15,6 +15,28 @@ import br.com.alura.gerenciador.dao.EmpresaDAO;
 @WebServlet(urlPatterns="/busca")
 public class BuscaEmpresa extends HttpServlet {
 	
+	
+	//A servlet é instanciada apenas uma vez
+	public BuscaEmpresa() {
+		System.out.println("Construindo uma servlet do tipo BuscaEmpresa: "+this);
+	}
+	
+	//logo após a criação do objeto, é feita inicialização, que também é somente uma vez
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("Inicializando a servlet: "+this);
+	}
+	
+	//destruido somente quando o servidor é parado
+	@Override
+	public void destroy() {
+		super.destroy();
+		System.out.println("Destruindo a servlet: "+this);
+	}
+	
+	
+	//como só vai ter uma instancia desse servlet, não devemos usar variáveis de classe(membro), pois elas podem entrar em concorrência
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
